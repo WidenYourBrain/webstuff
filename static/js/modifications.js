15e8e0c30aec52de5d04aa36a09cb42df6d8dcd4
Shalev Widen <shalevwiden@gmail.com>
2026-02-04 00:24:23 -0600

    More modifications to modifications.js

--- static/js/modifications.js
+++ static/js/modifications.js
@@ -1,3 +1,5 @@
+body = document.querySelector("body");
+
 console.log("modifications.js loaded");
 
 function spinbody() {
@@ -10,7 +12,6 @@ function spinbody() {
 // this function takes an argument "el" and applies the spin animation to whatever element
 // the element should already be queried and declared in javascript
 
-villasonriotext = document.querySelector("div.et_pb_text_inner > h1");
 function spinEl(el) {
   el.classList.add("spinclass");
   setTimeout(() => {
